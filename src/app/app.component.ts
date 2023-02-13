807bd4ec427e23fb4fd313ab6f8185ed2b92bcbf
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-13 13:10:36 -0300

    Realterando o titulo para deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Tutorial: Tour of Heroes';
+  title = 'Tour of Heroes';
 }
