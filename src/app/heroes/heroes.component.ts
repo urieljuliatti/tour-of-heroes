189a94fb753bfd3021fd0fc89efbeb79f96ef024
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-14 01:23:28 -0300

    Lista de heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock.heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
 
   hero: Hero = {
     id: 1,
     name: 'Wolverine'
   }
 
-  constructor() {}
+  heroes = HEROES;
 
-  ngOnInit(): void {
-
-  }
 }
---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,32 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Wolverine'
+  },
+  {
+    id: 2,
+    name: 'Thor'
+  },
+  {
+    id: 3,
+    name: 'Spider-man'
+  },
+  {
+    id: 4,
+    name: 'Batman'
+  },
+  {
+    id: 4,
+    name: 'Punisher'
+  },
+  {
+    id: 5,
+    name: 'Iron Man'
+  },
+  {
+    id: 6,
+    name: 'Superman'
+  }
+];
