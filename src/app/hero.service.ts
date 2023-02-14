b0d422a30f59e97edad23a1754fc1fe12586bac3
Uriel Valle <urielvalle@iMac-de-Uriel.local>
2023-02-14 15:50:37 -0300

    Update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HEROES } from './mock.heroes';
+import { Hero } from './hero';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   // A ideia do service é ser um singleton. Evitar que obtenhamos valores ou setemos nos componentes
   // O componente tem a missão de visualizar/exibir, não processar.
-  
-  constructor() { }
+
+  getHeroes(): Hero[]{
+    return HEROES;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock.heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit{
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
+
 }
