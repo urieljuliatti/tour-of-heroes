9f0607cb55804257c9d067f19eba818ac5f70245
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-14 15:59:34 -0300

    Adicionando Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock.heroes';
 import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   // A ideia do service é ser um singleton. Evitar que obtenhamos valores ou setemos nos componentes
   // O componente tem a missão de visualizar/exibir, não processar.
-
-  getHeroes(): Hero[]{
-    return HEROES;
+  // Observable que retorna uma lista de heroes para quando for feita uma chamada pro backend
+  getHeroes(): Observable<Hero[]>{
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,7 +24,9 @@ export class HeroesComponent implements OnInit{
   }
 
   getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes =>
+      this.heroes = heroes
+    );
   }
 
 }
